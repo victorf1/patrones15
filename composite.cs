6c6aac1cb41e5e7f1a4632f4c8b8f2b90eaae855
valekme <isc.valentin.nunez@gmail.com>
2015-02-13 16:47:06 -0800

    Composite
    Son varios programas dentro del Composite, que me toco exponer

---
+++ composite.cs
@@ -0,0 +1,84 @@
+//Empresa.cs
+
+using System;
+
+public abstract class Empresa
+{
+    protected static double costeUnitarioVehiculo = 5.0;
+    protected int nVehiculos;
+
+    public void agregaVehiculo()
+    {
+        nVehiculos = nVehiculos + 1;
+    }
+
+    public abstract double calculaCosteMantenimiento();
+
+    public abstract bool agregaFilial(Empresa filial);
+}
+
+//EmpresaMadre.cs
+
+using System;
+using System.Collections.Generic;
+
+public class EmpresaMadre : Empresa
+{
+    protected IList<Empresa> filiales =
+      new List<Empresa>();
+
+    public override bool agregaFilial(Empresa filial)
+    {
+        filiales.Add(filial);
+        return true;
+    }
+
+    public override double calculaCosteMantenimiento()
+    {
+        double cout = 0.0;
+        foreach (Empresa filial in filiales)
+            cout = cout + filial.calculaCosteMantenimiento();
+        return cout + nVehiculos * costeUnitarioVehiculo;
+    }
+}
+
+//EmpresaSinFilial.cs
+
+using System;
+
+public class EmpresaSinFilial : Empresa
+{
+    public override bool agregaFilial(Empresa filial)
+    {
+        return false;
+    }
+
+    public override double calculaCosteMantenimiento()
+    {
+        return nVehiculos * costeUnitarioVehiculo;
+    }
+}
+
+//Usuario.cs
+
+using System;
+
+public class Usuario
+{
+    static void Main(string[] args)
+    {
+        Empresa empresa1 = new EmpresaSinFilial();
+        empresa1.agregaVehiculo();
+        Empresa empresa2 = new EmpresaSinFilial();
+        empresa2.agregaVehiculo();
+        empresa2.agregaVehiculo();
+        Empresa grupo = new EmpresaMadre();
+        grupo.agregaFilial(empresa1);
+        grupo.agregaFilial(empresa2);
+        grupo.agregaVehiculo();
+        Console.WriteLine(
+          "Coste de mantenimiento total del grupo: " +
+          grupo.calculaCosteMantenimiento());
+    }
+}
+
